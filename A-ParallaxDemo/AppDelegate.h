dc5cad65b5124e53aa9fe954bc1f640c2fdc0b07
Animax Deng <Animax.my@gmail.com>
2015-08-18 23:34:30 -0500

    Initial submit

---
+++ A-ParallaxDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  A-Parallax
+//
+//  Created by Animax Deng on 8/18/15.
+//  Copyright (c) 2015 Animax Deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ A-ParallaxDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  A-Parallax
+//
+//  Created by Animax Deng on 8/18/15.
+//  Copyright (c) 2015 Animax Deng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
